e3a7506545ac834278eaa5a7028abf908b070881
javawebusingapi <maves951@gmail.com>
2023-08-14 17:50:15 +0900

    헤더 - 반응형 웹페이지 재구성

--- src/main/webapp/resources/javascript/header.js
+++ src/main/webapp/resources/javascript/header.js
@@ -1,38 +1,136 @@
-//검색기능
-function validation(){
-	if($("#headerSearchKeyword").val()==""){
-		alert("검색하고 싶은 내용을 입력하세요.");
-		console.log("키워드 없음");
-		return false;
-	}
-	console.log("정상 실행");
-	return true;
+window.onload = init;
+
+function init(){
+   console.log("init() 실행");
+   if(catehandle1.matches === true){
+      $("#menu-btn1").removeClass("d-block");
+      $("#menu-btn2").removeClass("d-block");
+      $("#menu-btn1").addClass("d-none");
+      $("#menu-btn2").addClass("d-none");
+      $("#menus").css("transform","translateX(0px)");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menu-btn2").css("backgroundImage","none");
+   }
+   
+   $("#carouselMainItem1").addClass("active");
+   $("#carouselCateMainItem1").addClass("active");
 }
-$(document).ready(function(){
-	$("#headerSearchBtn").click(function(){
-		var check = validation();
-	
-		if(check){
-			console.log("check 실행");
-			let searchValue = $("#headerSearchKeyword").val();
-			$("#headerSearchForm").attr("action","list?pname="+searchValue);
-			$("#headerSearchForm").submit();
-		}
-	});
-});
-	
+const catehandle1 = window.matchMedia(`(min-width: 1200px)`);
+const catehandle2 = window.matchMedia(`(max-width: 1200px)`);
+const catehandle3 = window.matchMedia(`(max-width: 992px)`);
+const catehandle4 = window.matchMedia(`(max-width: 768px)`);
+const catehandle5 = window.matchMedia(`(max-width: 575px)`);
+
 //헤더 메뉴 옆으로 넘기기
+
 function menuprev(){
-	$("#menu-btn2").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-left-gray@2x.png)");
-	$("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
-	$("#menus").css("transform","translateX(0px)")
-	$("#menu-btn2").attr("disabled",true);
-	$("#menu-btn1").attr("disabled",false);
+   if(catehandle2.matches === true){
+      $("#menu-btn2").css("backgroundImage","none");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menus").css("transform","translateX(0px)");
+   }
+   if(catehandle3.matches === true) {
+      $("#menu-btn2").css("backgroundImage","none");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menus").css("transform","translateX(0px)");
+   }
+   if(catehandle4.matches === true) {
+      $("#menu-btn2").css("backgroundImage","none");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menus").css("transform","translateX(0px)");
+   }
+   if(catehandle5.matches === true) {
+      $("#menus").css("transform","translateX(0px)");
+   }
 }
 function menunext(){
-	$("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right-gray@2x.png)");
-	$("#menu-btn2").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-left@2x.png)");
-	$("#menus").css("transform","translateX(-1150px)")
-	$("#menu-btn1").attr("disabled",true);
-	$("#menu-btn2").attr("disabled",false);
+   if(catehandle2.matches === true){
+      $("#menu-btn1").css("backgroundImage","none");
+      $("#menu-btn2").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-left@2x.png)");
+      $("#menus").css("transform","translateX(-150px)");
+   }
+   if(catehandle3.matches === true) {
+      $("#menu-btn1").css("backgroundImage","none");
+      $("#menu-btn2").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-left@2x.png)");
+      $("#menus").css("transform","translateX(-320px)");
+   }
+   if(catehandle4.matches === true) {
+      $("#menu-btn1").css("backgroundImage","none");
+      $("#menu-btn2").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-left@2x.png)");
+      $("#menus").css("transform","translateX(-480px)");
+   }
+   if(catehandle5.matches === true) {
+      $("#menus").css("transform","translateX(-300px)");
+   }
 }
+
+const viewChangeHandler1 = (catehandle1) => {
+   if(catehandle1.matches === true){
+      $("#menu-btn1").removeClass("d-block");
+      $("#menu-btn2").removeClass("d-block");
+      $("#menu-btn1").addClass("d-none");
+      $("#menu-btn2").addClass("d-none");
+      $("#menus").css("transform","translateX(0px)");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menu-btn2").css("backgroundImage","none");
+   } else {
+      $("#menu-btn1").removeClass("d-none");
+      $("#menu-btn2").removeClass("d-none");
+      $("#menu-btn1").addClass("d-block");
+      $("#menu-btn2").addClass("d-block");
+   }
+}
+
+const viewChangeHandler2 = (catehandle2) => {
+   if(catehandle2.matches === true){
+      $("#menus").css("transform","translateX(0px)");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menu-btn2").css("backgroundImage","none");
+   } else {
+      $("#menus").css("transform","translateX(0px)");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menu-btn2").css("backgroundImage","none");
+   }
+}
+
+const viewChangeHandler3 = (catehandle3) => {
+   if(catehandle3.matches === true){
+      $("#menus").css("transform","translateX(0px)");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menu-btn2").css("backgroundImage","none");
+   } else {
+      $("#menus").css("transform","translateX(0px)");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menu-btn2").css("backgroundImage","none");
+   }
+}
+
+const viewChangeHandler4 = (catehandle4) => {
+   if(catehandle4.matches === true){
+      $("#menus").css("transform","translateX(0px)");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menu-btn2").css("backgroundImage","none");
+   } else {
+      $("#menus").css("transform","translateX(0px)");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menu-btn2").css("backgroundImage","none");
+   }
+}
+
+const viewChangeHandler5 = (catehandle5) => {
+   if(catehandle5.matches === true){
+      $("#menus").css("transform","translateX(0px)");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menu-btn2").css("backgroundImage","none");
+   } else {
+      $("#menus").css("transform","translateX(0px)");
+      $("#menu-btn1").css("backgroundImage","url(//img1a.coupangcdn.com/image/coupang/common/pc_gnb_arrow-right@2x.png)");
+      $("#menu-btn2").css("backgroundImage","none");
+   }
+}
+
+catehandle1.addEventListener("change", viewChangeHandler1);
+catehandle2.addEventListener("change", viewChangeHandler2);
+catehandle3.addEventListener("change", viewChangeHandler3);
+catehandle4.addEventListener("change", viewChangeHandler4);
+catehandle5.addEventListener("change", viewChangeHandler5);
--- src/main/webapp/resources/javascript/main.js
+++ src/main/webapp/resources/javascript/main.js
@@ -1,36 +1,31 @@
-window.onload = init;
 
-function init(){
-	$("#carouselMainItem1").addClass("active");
-	$("#carouselCateMainItem1").addClass("active");
-}
 
 const cateAdvertiseHide = window.matchMedia(`(max-width: 1100px)`);
 const cateMainBannerHide = window.matchMedia(`(max-width: 768px)`);
 
-const viewChangeHandler1 = (cateAdvertiseHide) => {
-	if(cateAdvertiseHide.matches === true){
-		$("#cateAdvertise").addClass("disabled-div");
-		$("#cateAdvertise").removeClass("d-block");
-		$("#cateAdvertise").addClass("d-none");
-	} else {
-		$("#cateAdvertise").removeClass("disabled-div");
-		$("#cateAdvertise").removeClass("d-none");
-		$("#cateAdvertise").addClass("d-block");
-	}
+const changeHandler1 = (cateAdvertiseHide) => {
+   if(cateAdvertiseHide.matches === true){
+      $("#cateAdvertise").addClass("disabled-div");
+      $("#cateAdvertise").removeClass("d-block");
+      $("#cateAdvertise").addClass("d-none");
+   } else {
+      $("#cateAdvertise").removeClass("disabled-div");
+      $("#cateAdvertise").removeClass("d-none");
+      $("#cateAdvertise").addClass("d-block");
+   }
 }
 
-const viewChangeHandler2 = (cateMainBannerHide) => {
-	if(cateMainBannerHide.matches === true){
-		$("#mainproduct").addClass("disabled-div");
-		$("#mainproduct").removeClass("d-block");
-		$("#mainproduct").addClass("d-none");
-	} else {
-		$("#mainproduct").removeClass("disabled-div");
-		$("#mainproduct").removeClass("d-none");
-		$("#mainproduct").addClass("d-block");
-	}
+const changeHandler2 = (cateMainBannerHide) => {
+   if(cateMainBannerHide.matches === true){
+      $("#mainproduct").addClass("disabled-div");
+      $("#mainproduct").removeClass("d-block");
+      $("#mainproduct").addClass("d-none");
+   } else {
+      $("#mainproduct").removeClass("disabled-div");
+      $("#mainproduct").removeClass("d-none");
+      $("#mainproduct").addClass("d-block");
+   }
 }
 
-cateAdvertiseHide.addEventListener("change", viewChangeHandler1);
-cateMainBannerHide.addEventListener("change", viewChangeHandler2);
+cateAdvertiseHide.addEventListener("change", changeHandler1);
+cateMainBannerHide.addEventListener("change", changeHandler2);
