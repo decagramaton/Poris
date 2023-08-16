1b2f5d6922d2433c55c73744db831318789a756f
dwdwb <pipal1221@gmail.com>
2023-08-16 16:01:37 +0900

    git merge 테스트

--- src/main/java/poris/fruitlight/dto/OrderHistory.java
+++ src/main/java/poris/fruitlight/dto/OrderHistory.java
@@ -17,7 +17,7 @@ public class OrderHistory {
 	private Date ORDER_DATE;				//결제 날짜
 	private int TOTAL_PRICE;				//총 상품가격
 	private int DISCOUNT_PRICE;				//할인금액
-	private int SHIPPING_PRICE;				//배송비
+	private int SHIPPING_PRICE;			//배송비
 	private int PAYMENT_PRICE;				//결제금액
 	private String PAYMENT_TYPE;			//결제 유형(계좌이체, 신용/체크카드, 휴대폰, 무통장입금(가상계좌))
 	private String CASH_RECEIPT_PURPOSE;	//현금영수증 발행목적(소득공제, 지출증빙)
